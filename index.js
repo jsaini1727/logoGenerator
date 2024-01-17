849267ba9164ca13b427427e485684b2a6c26984
Jsaini1727 <jsaini1727@gmail.com>
2024-01-17 09:37:34 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const questions = require('./questions');
-const svgGenerator = require('./svgGenerator');
-const {svg} = require('@svgdotjs/svg.js')
+// const svgGenerator = require('./svgGenerator');
+// const {svg} = require('@svgdotjs/svg.js')
 // Prompt the user for questions regarding the 3 alphabets they want as the text for their logo
 inquirer.prompt(questions)
     .then((answers) => {
