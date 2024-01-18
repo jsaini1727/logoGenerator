ef44bd480cb5529f5d1dd5a9681d940fb4433dcc
Jsaini1727 <jsaini1727@gmail.com>
2024-01-18 15:24:37 -0500

    Created the various shapes and linked them to the index.js

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Circle = require('./lib/Circle.js');
+const Triangle = require('./lib/Triangle.js');
+const Square = require('./lib/Square.js');
 const questions = require('./questions');
 // const svgGenerator = require('./svgGenerator');
 // const {svg} = require('@svgdotjs/svg.js')
 // Prompt the user for questions regarding the 3 alphabets they want as the text for their logo
 inquirer.prompt(questions)
     .then((answers) => {
-        const userAnswers = answers;
-        const svgFile = svg(userAnswers);
+        let shape;
+        if (answer.shape === 'circle'){
+            shape = new Circle$
+        }
+        const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+        <circle cx="150" cy="100" r="80" fill="${this.shapeColor}"/>
+        <text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.textcolor}">${this.characters}</text></svg>`;
+
         // function to write the SVG file.
         fs.writeFile(logo.svg, svgFile, (err) => {
             if (err) {
